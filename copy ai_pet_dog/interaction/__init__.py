e2800116ee7e941a6cc5799527aad8365539ce1d
小芯 <149369235+xinling147@users.noreply.github.com>
2025-04-09 18:00:38 +0800

    Delete copy ai_pet_dog directory

--- copy ai_pet_dog/interaction/__init__.py
+++
@@ -1,7 +0,0 @@
-# 交互功能包初始化文件
-from .voice import VoiceRecognition
-from .touch import TouchInteraction
-from .emotion import EmotionSystem
-
-# 导出交互类
-__all__ = ['VoiceRecognition', 'TouchInteraction', 'EmotionSystem']
